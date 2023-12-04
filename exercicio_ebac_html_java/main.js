9d90cd182e0ae1a1191262b37ff87e4e92eabbc2
Alexandre Ramos <alexandre98rl@gmail.com>
2023-12-03 23:03:23 -0300

    xxxxx

--- exercicio_ebac_html_java/main.js
+++ exercicio_ebac_html_java/main.js
@@ -1,4 +1,4 @@
-const form =  document.getElementById("form-deposiito");
+const form =  document.getElementById('form-deposiito');
 
 function ValidaNome(nomecompleto) {
    const nomecomoarray = nomecompleto.split(' ');
@@ -6,13 +6,24 @@ function ValidaNome(nomecompleto) {
 }
 
 form.addEventListener('submit' , function (e) {
+    let formEValido = false;
     e.preventDefault();
 
     const nomebenficiario = document.getElementById('nome-benficiario');
-    if (ValidaNome(nomebenficiario.value) === false) {
-    alert("o nome não está completo");
+    const numerocontabenficiario = document.getElementById('Número-conta');
+    const Valor = document.getElementById('valor-deposito');
+    const mensagemSucesso = ' montante de: ${valor.value} depositado para cliente: ${nomebenficiario.value} - conta: ${numerocontabenficiario.value}';
+    
+    formEValido = ValidaNome(nomebenficiario.value)
+    if (formEValido) {
+        alert("mensagemSucesso");
+
+        nomebenficiario.value ='';
+        numerocontabenficiario.value = '';
+        valor.value = '';
+
     } else {
-        alert("tudo certo");
+        alert("o nome não está completo");
     }
 })
 
